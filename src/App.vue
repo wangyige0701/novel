8e852e701dab3dbe3a9d99f20bf4b1d266fc90b7
WangYige <1776686515@qq.com>
2023-12-23 22:20:45 +0800

    scss文件调整

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,7 @@ onError(err => {
 </script>
 
 <style lang="scss">
-@import './style.scss';
-@import './uni.scss';
+@import './static/scss/uni.scss';
 
 /* #ifndef APP-NVUE */
 * {
@@ -27,77 +26,5 @@ html {
 	font-size: $uni-font-size-base;
 }
 
-// 小说阅读页面展示
-#novel_content {
-	color: $wyg-font-default;
-	font-size: $uni-font-size-base;
-	&.baixue {
-		background-color: $wyg-bg-baixue;
-	}
-	&.huilv {
-		background-color: $wyg-bg-huilv;
-	}
-	&.qianlan {
-		background-color: $wyg-bg-qianlan;
-	}
-	&.minghuang {
-		background-color: $wyg-bg-minghuang;
-	}
-	&.danlv {
-		background-color: $wyg-bg-danlv;
-	}
-	&.caolv {
-		background-color: $wyg-bg-caolv;
-	}
-	&.hongfen {
-		background-color: $wyg-bg-hongfen;
-	}
-	&.fangmoshui {
-		color: $wyg-font-fangmoshui;
-		background-color: $wyg-bg-fangmoshui;
-	}
-	&.mihuang {
-		background-color: $wyg-bg-mihuang;
-	}
-	&.cha {
-		background-color: $wyg-bg-cha;
-	}
-	&.yin {
-		background-color: $wyg-bg-yin;
-	}
-	&.heilv {
-		color: $wyg-font-heilv;
-		background-color: $wyg-bg-heilv;
-	}
-	&.qianhuang {
-		background-color: $wyg-bg-qianhuang;
-	}
-	&.wuye {
-		color: $wyg-font-wuye;
-		background-color: $wyg-bg-wuye;
-	}
-	&.qianhui {
-		background-color: $wyg-bg-qianhui;
-	}
-	&.qihei {
-		color: $wyg-font-qihei;
-		background-color: $wyg-bg-qihei;
-	}
-	&.f_reduce1 {
-		font-size: 12px;
-	}
-	&.f_reduce2 {
-		font-size: 10px;
-	}
-	&.f_increase1 {
-		font-size: 16px;
-	}
-	&.f_increase2 {
-		font-size: 18px;
-	}
-	&.f_increase3 {
-		font-size: 20px;
-	}
-}
 /* #endif */
 </style>

ea335fbe43c9e23211680f0bb5ed22eff7a267f9
WangYige <1776686515@qq.com>
2024-01-05 22:54:52 +0800

    路径别名调整

--- src/common/request/main/proxy.ts
+++ src/common/request/main/proxy.ts
@@ -1,4 +1,4 @@
-import { ConfigTarget } from '@/config/data';
+import { ConfigTarget } from '../../../config/data'; // 注：此文件需要被vite.config.ts引用，所以不能使用路径别名
 
 export default {
 	test: ConfigTarget.test.domain,

35f383b2b98086b384f8908fc5b42b3ec93cf48b
WangYige <1776686515@qq.com>
2025-03-08 21:43:01 +0800

    引入vitest

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+import { defineConfig as viteDefineConfig } from 'vite';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+	viteDefineConfig({
+		resolve: {
+			...viteConfig.resolve,
+		},
+	}),
+	defineConfig({
+		test: {
+			name: 'node',
+			environment: 'node',
+			include: ['src/test/**/*.test.ts'],
+		},
+	}),
+);

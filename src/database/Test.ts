618f796d3fc752baab3f31cfd6ded24dbac56149
WangYige <1776686515@qq.com>
2025-04-01 23:14:49 +0800

    添加索引配置

--- src/database/Test.ts
+++ src/database/Test.ts
@@ -1,7 +1,17 @@
 import { Column, Id, Table, Type } from '@/common/database';
 import { BaseTable } from '@/common/database/base';
 
-@Table({ name: 'test', test: true }, '测试')
+@Table(
+	{
+		name: 'test',
+		test: true,
+		indexs: [
+			{ name: 'idx_level', columns: ['level_id'] },
+			{ name: 'idx_card', columns: ['id_card'], unique: true },
+		],
+	},
+	'测试',
+)
 export default class Test extends BaseTable<typeof Test> {
 	@Id()
 	@Column('主键')
@@ -10,9 +20,12 @@ export default class Test extends BaseTable<typeof Test> {
 	@Column({ name: 'name', type: Type.TEXT }, '姓名')
 	name: string;
 
-	/**
-	 * 性别，0：男，1：女
-	 */
-	@Column({ name: 'gender', type: Type.INTEGER, default: 0 }, '性别')
+	@Column({ name: 'gender', type: Type.INTEGER, nullable: false, default: 0 }, '性别；0：男，1：女')
 	gender: number;
+
+	@Column({ name: 'id_card', type: Type.TEXT, unique: true, nullable: false }, '身份证')
+	idCard: string;
+
+	@Column({ name: 'level_id', type: Type.INTEGER, nullable: true }, '等级')
+	levelId: number;
 }
